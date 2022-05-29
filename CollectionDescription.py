2f7dd2a6480c94d3e47872fa74d80070aeeb800c
Milos <josipovic.pr105.2019@uns.ac.rs>
2022-05-29 13:03:51 +0200

    klasa CollectionDescription

---
+++ CollectionDescription.py
@@ -0,0 +1,7 @@
+import HistoricalCollection.py
+
+class CollectionDescription:
+    def __init__(self, id, data_set, historical_collection):
+        self.id = id
+        self.data_set = data_set
+        self.historical_collection = historical_collection
