ec2fc59bd67b6931329bc0425fbe3a03b583af9a
Milos <josipovic.pr105.2019@uns.ac.rs>
2022-06-10 21:22:58 +0200

    Uproscavanje koda za writer

--- writer.py
+++ writer.py
@@ -1,7 +1,5 @@
-# writer-client.py
 
 import socket
-import codovi
 from time import sleep
 
 HOST = "127.0.0.1"
@@ -20,31 +18,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print("Enter desired Value:")
         value = input()
 
-        if code == -1 or value == "END":
+        if int(code) == -1 or value == "END":
             break
-
-        if code == 0:
-            data = codovi.Code.CODE_ANALOG + ";" + value
-        elif code == 1:
-            data = codovi.Code.CODE_DIGITAL + ";" + value
-        elif code == 2:
-            data = codovi.Code.CODE_CUSTOM + ";" + value
-        elif code == 3:
-            data = codovi.Code.CODE_LIMITSET + ";" + value
-        elif code == 4:
-            data = codovi.Code.CODE_SINGLENODE + ";" + value
-        elif code == 5:
-            data == codovi.Code.CODE_MULTIPLENODE + ";" + value
-        elif code == 6:
-            data = codovi.Code.CODE_CONSUMER + ";" + value
-        elif code == 7:
-            data = codovi.Code.CODE_SOURCE + ";" + value
-
+        
         sleep(2)
+        data = str(code) + ";" + str(value)
         s.sendall(data.encode('utf-8'))
-
 
         print("Writer has sent your data to the next destination")
 
-#data = "-1;END"
-#s.sendall(data)
+s.close(address)
+
