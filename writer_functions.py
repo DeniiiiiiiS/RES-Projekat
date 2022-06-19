51199922ad25b845cdf088e25aef40c693bad7d1
Milos <josipovic.pr105.2019@uns.ac.rs>
2022-06-19 11:58:50 +0200

    Preneta funkcija logger u zaseban fajl, radi testiranja

---
+++ writer_functions.py
@@ -0,0 +1,9 @@
+import json
+import time
+
+def logger(message):
+    time_now = time.localtime()
+    with open("writer.txt", 'a') as f:
+        a = f"{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}, {time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec}, {message}\n"
+        f.write(f"{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}, {time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec}, {message}\n")
+        return a
