b4f304f2e8fa22c6739a30c74c85bb5d95dfb21a
Milos <josipovic.pr105.2019@uns.ac.rs>
2022-06-19 21:01:13 +0200

    Implementacija unittestova za logger funkciju

---
+++ test_replicatorReceiver.py
@@ -0,0 +1,16 @@
+import unittest
+import time
+
+from pip import main
+from replicatorReceiver_functions import logger
+
+class TestLogg(unittest.TestCase):
+    def test_logger(self):
+        time_now = time.localtime()
+        self.assertEqual(logger("proba"), f"{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}, {time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec}, proba\n")
+        self.assertEqual(logger(6), f"{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}, {time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec}, 6\n")
+        self.assertEqual(logger(""), f"{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}, {time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec}, \n")
+        self.assertEqual(logger(object), f"{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}, {time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec}, {object}\n")
+
+if __name__ == '__main__':
+    unittest.main()
