4031c601caffd044a09ecaa6b59578eb11702c58
vlaksy <petrovic.pr102.2018@uns.ac.rs>
2022-06-20 03:40:57 +0200

    Prepravka koda (logger, check_deadband, insert, insert_process)

--- Reader2.py
+++ Reader2.py
@@ -1,10 +1,9 @@
 # Reader2-server.py
-
+import pickle
 import socket
 import mysql.connector
-import pickle
 from time import sleep
-import time
+from time import localtime
 from mysql.connector import Error
 
 import codovi
@@ -16,21 +15,21 @@ import receiverProperty
 HOST = "127.0.0.1"
 PORT = 8006
 NUMBER_OF_BYTES = 1000000
-time_now = time.localtime()
 
 
 def logger(message):
-    try:
-        with open("Reader2_Logger.txt", 'a') as file:
-            file.write(f"{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}, "
-                       f"{time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec} -> "
-                       f"{message}\n")
-    finally:
+    time_now = localtime()
+    with open("Reader2_Logger.txt", 'a') as file:
+        log = f"{time_now.tm_mday}.{time_now.tm_mon}.{time_now.tm_year}, " \
+            f"{time_now.tm_hour}:{time_now.tm_min}:{time_now.tm_sec}" \
+            f" -> {message}\n"
+        file.write(f"{log}")
         file.close()
+        return log
 
 
 # kreiranje DATABASE database_reader ako ne postoji
-def create_database():
+def connect_to_database():
     sqldb = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -43,9 +42,9 @@ def create_database():
         logger("Reader2 tried to create database that already exists, attempting to connect.")
         return print("Reader2: Database database_reader already exists, attempting to connect...")
     else:
-        print("Reader2: Creating database database_reader")
+        mycur.execute("create database database_reader")
         logger("Reader2 successfully created database: [database_reader].")
-        return mycur.execute("create database database_reader")
+        return print("Reader2: Creating database database_reader")
 
 
 # povezivanje na DATABASE database_reader
@@ -68,14 +67,63 @@ def mydb_connection(host_name, user_name, user_password):
 def create_table(connection):
     mycursor = connection.cursor()
     mycursor.execute("SELECT TABLE_NAME FROM information_schema.tables WHERE table_name = 'tabledata2'")
-    result = mycursor.fetchall()
-    if not result:
+    myresult = mycursor.fetchall()
+    if not myresult:
         mycursor.execute("create table tabledata2(id int, dataset int, code varchar(20), value int, date datetime "
                          "PRIMARY KEY default now())")
         logger("Reader2 successfully created table: [tabledata2].")
         print("Reader2: Creating tabledata2")
+        return "Table created and ready to use!"
     else:
         print("Reader2: Table tabledata2 already exists, ready to use")
+        return "Table ready to use!"
+
+
+# funkcija koja proverava da li je code code_digital
+def insert_process(connection, id_data, dataset, code_number, value):
+    code = codovi.Code(code_number).name
+    if code == 'CODE_DIGITAL':
+        print(f"Reader2: Code is '{code}', inserting data into table tabledata2")
+        logger("Reader2 successfully executed function: [insert_process].")
+        return insert(connection, id_data, dataset, code, value)
+    else:
+        print("Reader2: Code isn't CODE_DIGITAL, checking deadband...")
+        logger("Reader2 successfully executed function: [insert_process].")
+        return check_deadband(connection, id_data, dataset, code, value)
+
+
+# funkcija koja proverava deadband uslov
+def check_deadband(connection, id_data, dataset, code, value):
+    mycursor = connection.cursor()
+    mycursor.execute(f"select value from tabledata2 where code = '{code}'")
+    myresult = mycursor.fetchall()
+    if not myresult:
+        print("Reader2: Code does not exist in table, inserting data")
+        logger("Reader2 successfully executed function: [check_deadband].")
+        return insert(connection, id_data, dataset, code, value)
+    i = 0
+    for row in myresult:
+        if (abs(row[0] - value) / ((row[0] + value) / 2)) * 100 > 2:
+            i += 1
+    if i == myresult.__len__():
+        print(f"Reader2: Difference between {value} and values in database is "
+              f"greater than 2%, inserting data into table tabledata2")
+        logger("Reader2 successfully executed function: [check_deadband].")
+        return insert(connection, id_data, dataset, code, value)
+    else:
+        logger("Reader2 successfully executed function: [check_deadband].")
+        return print("Reader2: No insertion, difference between values is less than 2%")
+
+
+# funkcija koja upisuje u tabelu podatke
+def insert(connection, id_data, dataset, code, value):
+    mycursor = connection.cursor()
+    mycursor.execute(
+        f"insert into tabledata2(id, dataset, code, value, date) "
+        f"values ({id_data}, {dataset}, '{code}', {value}, now())")
+    connection.commit()
+    logger("Reader2 successfully executed function: [insert].")
+    return sleep(1)
 
 
 # funkcija za dobavljanje poslednje vrednosti za izabrani code
@@ -101,96 +149,53 @@ def read_values_by_code(connection, code_number):
     if not myresult:
         print(f"Reader2: Given code ['{code}'] does not exist in the table")
     else:
-        print("Reader2: ID |///| DATASET |///| CODE          |///| VALUE |///| DATE")
+        print("Reader2: ID |///| DATASET |///| CODE              |///| VALUE |///| DATE       TIME")
         for x in myresult:
-            print(f"Reader2: {x[0]}        {x[1]}             {x[2]}         {x[3]}           {x[4]}")
+            print(f'Reader1: {f"{x[0]}":<9}{f"{x[1]}":<14}{f"{x[2]}":<24}{f"{x[3]}":<12}{f"{x[4]}":<10}')
     logger("Reader2 successfully executed function: [read_values_by_code].")
 
 
-# funkcija koja proverava da li je code code_digital
-def insert_process(connection, id_data, dataset, code_number, value):
-    code = codovi.Code(code_number).name
-    if code == "CODE_DIGITAL":
-        print(f"Reader2: Code is '{code}', inserting data into table tabledata2")
-        logger("Reader2 successfully executed function: [insert_process].")
-        return insert(connection, id_data, dataset, code, value)
-    else:
-        print("Reader2: Code isn't CODE_DIGITAL, checking deadband...")
-        logger("Reader2 successfully executed function: [insert_process].")
-        return check_deadband(connection, id_data, dataset, code, value)
+def start_reader2():
+    connect_to_database()
+    connection = mydb_connection("localhost", "root", "root")
+    logger("Reader2 successfully connected to database.")
+    create_table(connection)
 
+    # povezivanje sa replicator receiver-om
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print("Reader2: Waiting for connection...")
+        logger("Reader2 waiting for connection.")
+        conn, addr = s.accept()
+        with conn:
+            print(f"Reader2: Replicator receiver connected from {addr}")
+            logger("ReplicatorReceiver successfully connected to Reader2.")
+            while True:
+                inc_data = conn.recv(NUMBER_OF_BYTES)
+                data = pickle.loads(inc_data)
+                logger("Reader2 successfully received data from replicatorReceiver.")
+                add_lista = data.add_list
+                update_lista = data.update_list
 
-# funkcija koja proverava deadband uslov
-def check_deadband(connection, id_data, dataset, code_number, value):
-    code = codovi.Code(code_number).name
-    mycursor = connection.cursor()
-    mycursor.execute(f"select value from tabledata2 where code = '{code}'")
-    myresult = mycursor.fetchall()
-    i = 0
-    for row in myresult:
-        if (abs(row[0] - value) / ((row[0] + value) / 2)) * 100 > 2:
-            i += 1
-    if i >= 1:
-        print("Reader2: Difference between values is greater than 2%, inserting data into table tabledata2")
-        logger("Reader2 successfully executed function: [check_deadband].")
-        return insert(connection, id_data, dataset, f"'{code}'", value)
-    else:
-        logger("Reader2 successfully executed function: [check_deadband].")
-        print("Reader2: No insertion, difference between values is less than 2%")
+                # upisivanje vrednosti u tabelu iz add_list-e
+                logger("Reader2 started reading data from add_list.")
+                for cdx in add_lista:
+                    id_add = cdx.getId()
+                    dataset_add = cdx.getDataset()
+                    hc_add = cdx.getHistoricalCollection().getNiz()
+                    for cdy in hc_add:
+                        code_add = cdy.getCode()
+                        value_add = cdy.getValue()
+                        insert_process(connection, id_add, dataset_add, code_add, value_add)
 
-
-# funkcija koja upisuje u tabelu podatke
-def insert(connection, id_data, dataset, code_number, value):
-    code = codovi.Code(code_number).name
-    mycursor = connection.cursor()
-    mycursor.execute(
-        f"insert into tabledata2(id, dataset, code, value, date) "
-        f"values ({id_data}, {dataset}, '{code}', {value}, now())")
-    connection.commit()
-    logger("Reader2 successfully executed function: [insert].")
-    return sleep(1)
-
-
-create_database()
-connect = mydb_connection("localhost", "root", "root")
-logger("Reader2 successfully connected to database.")
-create_table(connect)
-
-# povezivanje sa replicator receiver-om
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print("Reader2: Waiting for connection...")
-    logger("Reader2 waiting for connection.")
-    conn, addr = s.accept()
-    with conn:
-        print(f"Reader2: Replicator receiver connected from {addr}")
-        logger("ReplicatorReceiver successfully connected to Reader2.")
-        while True:
-            inc_data = conn.recv(NUMBER_OF_BYTES)
-            data = pickle.loads(inc_data)
-            logger("Reader2 successfully received data from replicatorReceiver.")
-            add_lista = data.add_list
-            update_lista = data.update_list
-
-            # upisivanje vrednosti u tabelu iz add_list-e
-            logger("Reader2 started reading data from add_list.")
-            for cdx in add_lista:
-                id_add = cdx.getId()
-                dataset_add = cdx.getDataset()
-                hc_add = cdx.getHistoricalCollection().getNiz()
-                for cdy in hc_add:
-                    code_add = cdy.getCode()
-                    value_add = cdy.getValue()
-                    insert_process(connect, id_add, dataset_add, code_add, value_add)
-
-            # upisivanje vrednosti u tabelu iz update_list-e
-            logger("Reader2 started reading data from update_list.")
-            for cdx in update_lista:
-                id_update = cdx.getId()
-                dataset_update = cdx.getDataset()
-                hc_update = cdx.getHistoricalCollection().getNiz()
-                for cdy in hc_update:
-                    code_update = cdy.getCode()
-                    value_update = cdy.getValue()
-                    insert_process(connect, id_update, dataset_update, code_update, value_update)
+                # upisivanje vrednosti u tabelu iz update_list-e
+                logger("Reader2 started reading data from update_list.")
+                for cdx in update_lista:
+                    id_update = cdx.getId()
+                    dataset_update = cdx.getDataset()
+                    hc_update = cdx.getHistoricalCollection().getNiz()
+                    for cdy in hc_update:
+                        code_update = cdy.getCode()
+                        value_update = cdy.getValue()
+                        insert_process(connection, id_update, dataset_update, code_update, value_update)
