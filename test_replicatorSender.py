0e5c49c02fd382c8d14d42528c52cafce9452d92
vlaksy <petrovic.pr102.2018@uns.ac.rs>
2022-06-23 00:08:18 +0200

    Prepravka koda

--- test_replicatorSender.py
+++ test_replicatorSender.py
@@ -1,23 +1,22 @@
 import unittest
 import time
-import receiverProperty
-from sender_functions import konvertuj_u_ReceiverProperty
-from sender_functions import Logger
+from sender_functions import konvertuj_u_receiver_property
+from sender_functions import logger
+
 
 class TestSender(unittest.TestCase):
-
     def test_Logger(self):
         vreme = time.localtime()
-        self.assertEqual(Logger("poruka"),f"{vreme.tm_mday}.{vreme.tm_mon}.{vreme.tm_hour}, {vreme.tm_hour}:{vreme.tm_min}:{vreme.tm_sec}, "+ "poruka"+"\n")
-        self.assertEqual(Logger(3),f"{vreme.tm_mday}.{vreme.tm_mon}.{vreme.tm_hour}, {vreme.tm_hour}:{vreme.tm_min}:{vreme.tm_sec}, {3}\n")
-        self.assertEqual(Logger(object),f"{vreme.tm_mday}.{vreme.tm_mon}.{vreme.tm_hour}, {vreme.tm_hour}:{vreme.tm_min}:{vreme.tm_sec}, {object}\n")
+        self.assertEqual(logger("poruka"), f"{vreme.tm_mday}.{vreme.tm_mon}.{vreme.tm_hour}, {vreme.tm_hour}:{vreme.tm_min}:{vreme.tm_sec}, "+ "poruka"+"\n")
+        self.assertEqual(logger(3), f"{vreme.tm_mday}.{vreme.tm_mon}.{vreme.tm_hour}, {vreme.tm_hour}:{vreme.tm_min}:{vreme.tm_sec}, {3}\n")
+        self.assertEqual(logger(object), f"{vreme.tm_mday}.{vreme.tm_mon}.{vreme.tm_hour}, {vreme.tm_hour}:{vreme.tm_min}:{vreme.tm_sec}, {object}\n")
 
     def test_konvertuj_u_ReceiverProperty(self):
-        self.assertEqual(konvertuj_u_ReceiverProperty(""),"lose")
-        self.assertEqual(konvertuj_u_ReceiverProperty(3),"lose")
-        self.assertEqual(konvertuj_u_ReceiverProperty(5.77), "lose")
-        self.assertEqual(konvertuj_u_ReceiverProperty(object), "lose")
-        self.assertEqual(konvertuj_u_ReceiverProperty("3;3;"), "lose")
+        self.assertEqual(konvertuj_u_receiver_property(""), "lose")
+        self.assertEqual(konvertuj_u_receiver_property(3), "lose")
+        self.assertEqual(konvertuj_u_receiver_property(5.77), "lose")
+        self.assertEqual(konvertuj_u_receiver_property(object), "lose")
+        self.assertEqual(konvertuj_u_receiver_property("3;3;"), "lose")
 
 
 if __name__ == '__main__':
